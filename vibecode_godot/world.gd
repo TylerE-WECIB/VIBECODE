2a42f3d38e8b51291f43cc3b454aa54b01f6b701
Tyler E <145382438+TylerE-WECIB@users.noreply.github.com>
2025-04-07 08:51:47 -0400

    bunch of code

--- vibecode_godot/world.gd
+++ vibecode_godot/world.gd
@@ -1,11 +1,32 @@
 extends Node2D
-
+var time_to_press := 3.0
+var wanted_key := "a"
+var score := 0
+var keyboard_keys = [
+	"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+	"[", "]", ";", "'", ".", ",", "-", "=",
+	"up", "down", "left", "right"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Crowd.play()
+	$EarthGone.visible = false
+	randomize()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	$"QTE Timer".start(time_to_press)
+	wanted_key = keyboard_keys.pick_random()
+	$ButtonToPress.text = wanted_key
+	while not Input.is_action_just_pressed(wanted_key):
+		pass
+	if not $"QTE Timer".is_stopped():
+		score += 1
+		if score % 5 == 0:
+			time_to_press = clamp(time_to_press - 0.5, 0.1, time_to_press)
+			print(time_to_press)
+
+func _on_qte_timer_timeout() -> void:
+	$AnimationPlayer.play("punch")
